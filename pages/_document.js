6db382746047eea42c5ac5271f62b936d31ac424
Spandan <spandansehgal@gmail.com>
2023-10-21 18:04:35 +0530

    second commit

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,9 @@ import Navbar from '@/components/navbar'
 export default function Document() {
   return (
     <Html lang="en">
-        <Head/>
-
+        
         <body>
+          
         <Navbar/>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Image from 'next/image'
+// import Image from 'next/image'
+
 import { Inter } from 'next/font/google'
 import DictionarySection from '@/components/DictionarySection'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
